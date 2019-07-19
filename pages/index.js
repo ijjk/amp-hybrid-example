93826d37111722e3699aa3a83e7f23c39a18dd05
JJ Kasper <jj@jjsweb.site>
2019-07-19 14:03:57 -0500

    Update to latest Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import { withAmp, useAmp } from 'next/amp'
+import { useAmp } from 'next/amp'
 import AmpPage from '../components/amp'
 import NonAmpPage from '../components/non-amp'
 
-export default withAmp(() => (
+export const config = { amp: 'hybrid' }
+
+export default () => (
   useAmp() ? <AmpPage /> : <NonAmpPage />
-), { hybrid: true })
+)
